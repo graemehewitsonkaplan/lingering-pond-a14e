23eb41ef5e3bf09877fc582956f49cf48158f358
Graeme Hewitson <150795679+graemehewitsonkaplan@users.noreply.github.com>
2025-02-27 16:42:34 +0000

    Update hello.tsx

--- src/app/components/hello.tsx
+++ src/app/components/hello.tsx
@@ -2,7 +2,7 @@ export default function Hello() {
   return (
     <div>
       <h1>My Custom Next.js Page</h1>
-      <p>This replaces the default content.</p>
+      <p>This replaces the default content.</p> 
     </div>
   );
 }
